6c6b1a4b9d196c417a8d5f8d16d583e15eefb2c9
kodai <kodai@10.0.0.249>
2023-12-18 15:15:10 -0500

    filter class

--- src/test/java/com/thinktank/utilities/Filter.java
+++ src/test/java/com/thinktank/utilities/Filter.java
@@ -1,22 +1,39 @@
 package com.thinktank.utilities;
 
 public class Filter {
-	
-	public static String stripword(String ingrientname) {
-		String [] wordstoRemove={"powdered","\\(",")//"};	
-		for (String word:wordstoRemove) {
-			if(ingrientname.contains(word)) {
-				ingrientname=ingrientname.replace(word, "");
-				ingrientname=ingrientname.toLowerCase().trim();
-			}
-		}
-		
-		System.out.println(ingrientname);
-		return ingrientname;
-		
-	}
-	public static void main(String[] args) {
-		stripword("(maida)");
-	}
 
+    public static String stripword(String ingredientName) {
+        String[] wordsToRemove = {
+            "powdered", "\\(", "\\)", "black", "grated mozzarella", "sea",
+            "grated", "grated processed", "chopped", "diced mozzarella", "icing",
+            "sweet and sour", "red chilli", "chilli garlic", "baby","powder"
+        };
+
+        for (String word : wordsToRemove) {
+            if (ingredientName.contains(word)) {
+                ingredientName = ingredientName.replace(word, "");
+            }
+        }
+
+        ingredientName = ingredientName.toLowerCase().trim();
+        System.out.println(ingredientName);
+        return ingredientName;
+    }
+
+    public static void main(String[] args) {
+        stripword("(maida)");
+        stripword("powdered sugar");
+        stripword("black salt");
+        stripword("grated mozzarella cheese");
+        stripword("sea salt");
+        stripword("grated processed cheese");
+        stripword("chopped spinach");
+        stripword("diced mozzarella cheese");
+        stripword("icing sugar");
+        stripword("sweet and sour sauce");
+        stripword("red chilli sauce");
+        stripword("chilli garlic sauce");
+        stripword("baby corn");
+        stripword("coffee powder");
+    }
 }
